f62c3efb5f5f9bb2ac449293ecff65c0434bd84f
jordelucas <jordevalucas@hotmail.com>
2018-11-10 10:09:58 -0300

    arquivos para as tabelas com nome fornecido pelo usuário

--- criarTabela.c
+++ criarTabela.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
+#include <string.h> //para usar a função que concatena strings
 
 void criarTabela() {
 	FILE *pont_dados_tabela;
 	FILE *pont_tamanho_tabela;
+	char nome_dados_tabela[35] = "";
+	char nome_tamanho_tabela[35] = "";
+	char nome[15];
 
-	char nome_tabela[15];
 	printf("Nome: ");
-	scanf("%s", nome_tabela);
+	scanf("%s", nome);
 
-	pont_dados_tabela = fopen(("%s.txt", nome_tabela), "w");
+	strcat(nome_dados_tabela, "dados/");
+	strcat(nome_dados_tabela, nome);
+	strcat(nome_dados_tabela, "_dados.txt");
+	strcat(nome_tamanho_tabela, "tamanhos/");
+	strcat(nome_tamanho_tabela, nome);
+	strcat(nome_tamanho_tabela, "_tamanho.txt");
+
+	//Abrir arquivo de dados
+	pont_dados_tabela = fopen(nome_dados_tabela, "w");
 
 	if (pont_dados_tabela == NULL){
-    		printf("ERRO! O arquivo não foi aberto!\n");
+    		printf("ERRO! O arquivo de dados não foi aberto!\n");
 	}else{
-     		printf("O arquivo foi aberto com sucesso!");
+     		printf("O arquivo de dados foi aberto com sucesso!\n");
    	}
 
 	fclose(pont_dados_tabela);
+
+	//Abrir arquivo do tamanho dos atributos
+	pont_tamanho_tabela = fopen(nome_tamanho_tabela, "w");
+
+	if (pont_tamanho_tabela == NULL){
+				printf("ERRO! O arquivo dos tamanhos não foi aberto!\n");
+	}else{
+				printf("O arquivo dos tamanhos foi aberto com sucesso!\n");
+		}
+
+	fclose(pont_tamanho_tabela);
 }
