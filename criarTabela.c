3013743398cbcf40b2101a6c564fd806c34fd8cf
jordelucas <jordevalucas@hotmail.com>
2018-11-16 07:43:07 -0300

    correção no arquivamento dos nomes das tabelas

--- criarTabela.c
+++ criarTabela.c
@@ -12,38 +12,50 @@ struct tamanhoAtr{
 void criarTabela() {
 	FILE *pont_dados_tabela;
 	FILE *pont_tamanho_tabela;
+ 	FILE *pont_tabelas;
 	char nome_dados_tabela[35] = "";
 	char nome_tamanho_tabela[35] = "";
 	char nome[15];
 
+	printf("----CRIAR TABELA----\n\n");
+
+	fflush(stdin);
 	printf("Nome da tabela: ");
 	scanf("%s", nome);
 
-	strcat(nome_dados_tabela, "dados/");
+	strcat(nome_dados_tabela, "dados//");
 	strcat(nome_dados_tabela, nome);
 	strcat(nome_dados_tabela, "_dados.txt");
-	strcat(nome_tamanho_tabela, "tamanhos/");
+	strcat(nome_tamanho_tabela, "tamanhos//");
 	strcat(nome_tamanho_tabela, nome);
 	strcat(nome_tamanho_tabela, "_tamanho.txt");
 
+	//Adicionar nova tabela a lista
+	pont_tabelas = fopen("tabelas//listaTabelas.txt", "ab");
+
+	if (pont_tabelas == NULL){
+		printf("ERRO! O arquivo não foi aberto!\n");
+	}else{
+		fprintf(pont_tabelas, "%s\n", nome);
+	}
+
+	fclose(pont_tabelas);
+
 	//Criar arquivo de dados
-	pont_dados_tabela = fopen(nome_dados_tabela, "w");
+	pont_dados_tabela = fopen(nome_dados_tabela, "ab");
 
 	if (pont_dados_tabela == NULL){
   	printf("ERRO! O arquivo de dados não foi aberto!\n");
-	}else{
-  	printf("O arquivo de dados foi aberto com sucesso!\n");
-  }
+	}
 
 	fclose(pont_dados_tabela);
 
 	//Criar arquivo dos tamanhos e definir atributos atributos
-	pont_tamanho_tabela = fopen(nome_tamanho_tabela, "w");
+	pont_tamanho_tabela = fopen(nome_tamanho_tabela, "ab");
 
 	if (pont_tamanho_tabela == NULL){
 		printf("ERRO! O arquivo dos tamanhos não foi aberto!\n");
 	}else{
-		printf("O arquivo dos tamanhos foi aberto com sucesso!\n");
 		adicionarAtributo(pont_tamanho_tabela);
 	}
 
@@ -59,13 +71,21 @@ void adicionarAtributo (FILE * tabela){
 //Adicionando atributos
 	do {
 		if(qtdAtr == 1){
-			printf("Nome da chave primária: ");
+			fflush(stdin);
+			printf("\nNome da chave primária: ");
 			scanf("%s", atributo.nome);
 			atributo.tamanho = 4;
+
 		}else{
+
+			cabecalho();
+			printf("----CRIAR TABELA----\n\n");
+
+			fflush(stdin);
 			printf("\nNome do atributo: ");
 			scanf("%s", atributo.nome);
 
+			fflush(stdin);
 			printf("\n--TIPOS DE DADOS--\n");
 			printf("1- Char\n");
 			printf("2- Int\n");
@@ -96,10 +116,10 @@ void adicionarAtributo (FILE * tabela){
 					break;
 			}
 		}
-
 		fwrite(&atributo, sizeof(Tamanho_Atributo), 1, tabela);
 		qtdAtr++;
-		printf("Deseja continuar adicionando atributos(s/n)?");
-		scanf("%c", &continuar);
-	} while(continuar != 's');
+		fflush(stdin);
+		printf("\nDeseja continuar adicionando atributos(s/n)?");
+		scanf(" %c", &continuar);
+	}while(continuar != 'n');
 }
