d517fec70f3f6fa91e0cf6f28b898af40ac1c5c9
jordelucas <jordevalucas@hotmail.com>
2018-11-26 21:38:23 -0300

    alocacao dinamica do arquivo listarTabelas

--- listarTabelas.c
+++ listarTabelas.c
@@ -1,34 +1,61 @@
-void listarTabelas(){
+int listarTabelas(){
+  cabecalho();
   FILE * pont_tabelas;
-  char * texto_str = malloc(20*sizeof(char));
-  char resp;
+
+  char * texto_str = (char*) malloc(21*sizeof(char));                //reserva memória para identificação da tabela localizada no arquivo
+  if(texto_str == NULL){
+		printf("ERRO! Memória não alocada.");
+		exit(0);
+	}
+
+  char respSN;                                                //variável para respostas Sim ou Não do usuário
+
 	printf("---LISTAR TABELAS---\n\n");
 
   pont_tabelas = fopen("tabelas//listaTabelas.txt", "r");
   if (pont_tabelas == NULL){
 		printf("ERRO! O arquivo de nomes não foi aberto!\n");
+    exit(0);
 	}else{
-    if(fgets(texto_str, 20, pont_tabelas) == NULL){
+    if(fgets(texto_str, 21, pont_tabelas) == NULL){           //Não havendo nada na primeira linha do arquivo, significa dizer que não há tabelas cadastradas
       printf("Não há tabelas cadastradas!\n");
-      printf("Deseja cadastrar uma tabela(s/n)?");
-      scanf(" %c", &resp);
-      if (resp == 's') {
-        cabecalho();
+      fflush(stdin);
+      printf("Deseja cadastrar uma tabela(s/n)? ");
+      scanf(" %c", &respSN);
+      if (respSN == 's') {
+        fclose(pont_tabelas);
+        texto_str = NULL;
+        free(texto_str);
+        pont_tabelas = NULL;
+        free(pont_tabelas);
         criarTabela();
-        cabecalho();
+        return -1;
+      }else{
+        fclose(pont_tabelas);
+        texto_str = NULL;
+        free(texto_str);
+        pont_tabelas = NULL;
+        free(pont_tabelas);
+        return -1;
       }
-    }else{
+    }else{                                                    //Havendo linhas, são listados os nomes das tabelas
       do{
         printf("-> %s", texto_str);
-      }while (fgets(texto_str, 20, pont_tabelas) != NULL);
+      }while (fgets(texto_str, 21, pont_tabelas) != NULL);
+      fflush(stdin);
       printf("\nDeseja editar uma tabela(s/n)?");
-      scanf(" %c", &resp);
-      if (resp == 's') {
+      scanf(" %c", &respSN);
+      if (respSN == 's') {
         printf("OK\n");
       }else{
         printf("Blz\n");
       }
     }
 	}
-	fclose(pont_tabelas);
+  fclose(pont_tabelas);
+  texto_str = NULL;
+  free(texto_str);
+  pont_tabelas = NULL;
+  free(pont_tabelas);
+  return 0;
 }
