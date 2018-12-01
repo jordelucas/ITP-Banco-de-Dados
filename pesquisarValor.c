a38cd91d946d7ae99e1bb79036201ffdf52b13b7
jordelucas <jordevalucas@hotmail.com>
2018-12-01 12:34:25 -0300

    comentários e finalização da pesquisa de dados (pesquisarValor.c)

--- pesquisarValor.c
+++ pesquisarValor.c
@@ -1,28 +1,27 @@
 int pesquisarValor(){
 	cabecalho();
-	/*ponteiros para o tipo arquivo*/
+	/*--Ponteiros para o tipo arquivo--*/
 	FILE *pont_tabelas;
-	FILE *pont_temporario;
 	FILE *pont_dados_tabela;
 	FILE *pont_tamanho_tabela;
 
-	/*ponteiros para identificação de onde se encontram os arquivos da tabela solicitada*/
+	/*--Ponteiros para identificação de onde se encontram os arquivos da tabela solicitada--*/
 	char * dados;
 	char * tamanho;
 
-	char * nomeTabela = (char*)malloc(21*sizeof(char));								//reserva memória para identificação da tabela localizada no arquivo
+	char * nomeTabela = (char*)malloc(21*sizeof(char));														//Reserva memória para identificação dos nomes de tabelas contidos no arquivo arquivo
 	if(nomeTabela == NULL){
 		printf("ERRO! Memória não alocada.");
 		exit(0);
 	}
 
-	char * tabelaEscolhida = (char*)malloc(21*sizeof(char));						//reserva memória para identificação da tabela que receberá uma linha
+	char * tabelaEscolhida = (char*)malloc(21*sizeof(char));											//Reserva memória para identificação da tabela escolhida
 	if(tabelaEscolhida == NULL){
 		printf("ERRO! Memória não alocada.");
 		exit(0);
 	}
 
-	char * nomeAtributoEscolhido = (char*)malloc(21*sizeof(char));
+	char * nomeAtributoEscolhido = (char*)malloc(21*sizeof(char));								//Reserva memória para identificação do atributo escolhida para pesquisar valor
 	if(nomeAtributoEscolhido == NULL){
 		printf("ERRO! Memória não alocada.");
 		exit(0);
@@ -30,14 +29,14 @@ int pesquisarValor(){
 
 	int tipoAtributoEscolhido;
 
-	char respSN;																	//variável para respostas Sim ou Não do usuário
+	char respSN;																																	//Variável para respostas Sim ou Não do usuário
 
-	/*Variáveis auxiliades para leitura de arquivos*/
+	/*--Variáveis auxiliades para leitura de arquivos--*/
 	int linha_escolhida = 0;
 	int ordem;
 	int linha_atual = 1;
 
-	/*Contadores de tipos de dados dos atributos da tabela*/
+	/*-Contadores de tipos de dados dos atributos da tabela--*/
 	int _inteiro = 0;
 	int _double = 0;
 	int _char = 0;
@@ -50,14 +49,14 @@ int pesquisarValor(){
 
 	printf("---PESQUISAR VALOR--\n\n");
 
-	/*Lista das tabelas cadastradas*/
+	/*--Lista das tabelas cadastradas--*/
 	do{
 		pont_tabelas = fopen("tabelas//listaTabelas.txt", "r");
 		if (pont_tabelas == NULL){
 			printf("ERRO! O arquivo de nomes não foi aberto.\n");
 			exit(0);
 		}else{
-			if(fgets(nomeTabela, 20, pont_tabelas) == NULL){						//Não havendo nada na primeira linha do arquivo, significa dizer que não há tabelas cadastradas
+			if(fgets(nomeTabela, 20, pont_tabelas) == NULL){													//Não havendo nada na primeira linha do arquivo, significa dizer que não há tabelas cadastradas
 				printf("Não há tabelas cadastradas!\n");
 				printf("Deseja cadastrar uma tabela(s/n)? ");
 				fflush(stdin);
@@ -78,7 +77,7 @@ int pesquisarValor(){
 					tabelaEscolhida = NULL;
 					return -1;
 				}
-			}else{																	//Havendo linhas, são listados os nomes das tabelas com um indice ao lado
+			}else{																																		//Havendo linhas, são listados os nomes das tabelas com um indice ao lado
 				ordem = 0;
 				do {
 					printf("%d. %s", ++ordem, nomeTabela);
@@ -86,7 +85,7 @@ int pesquisarValor(){
 				fflush(stdin);
 				printf("\nEm qual tabela você deseja pesquisar valor? ");
 				scanf("%d", &linha_escolhida);
-				if (linha_escolhida < 1 || linha_escolhida > ordem) {				//Controle de que o indice solicitado seja um dos exibidos na tela
+				if (linha_escolhida < 1 || linha_escolhida > ordem) {										//Controle de que o indice solicitado seja um dos exibidos na tela
 					cabecalho();
 					printf("---PESQUISAR VALOR--\n\n");
 					printf("*Opção Inválida!\n");
@@ -99,18 +98,18 @@ int pesquisarValor(){
 	} while (linha_escolhida < 1 || linha_escolhida > ordem);
 
 
-	/*Localização dos arquivos da tabela solicidata*/
+	/*--Localização dos arquivos da tabela solicidata--*/
 	pont_tabelas = fopen("tabelas//listaTabelas.txt", "r");
 	if (pont_tabelas == NULL){
 		printf("ERRO! O arquivo de nomes não foi aberto.\n");
 		exit(0);
 	}else{
 		while(fgets(nomeTabela, 20, pont_tabelas) != NULL){
-			if (linha_atual == linha_escolhida) {									//Busca pelo indice que representa o nome da tabela escolhida
+			if (linha_atual == linha_escolhida) {											//Busca pelo indice que representa o nome da tabela escolhida
 				nomeTabela[strcspn(nomeTabela, "\n")] = 0;							//retira o '\n'
-				strcpy(tabelaEscolhida, nomeTabela);								//Guarda-se o nome da tabale escolhida
-				dados = diretorioDados(nomeTabela);									//Obtemos o respectivo diretório do arquivo com os dados da tabale
-				tamanho = diretorioTamanhos(nomeTabela);							//Obtemos o respectivo diretório do arquivo com os tamanhos dos tipos de dados da tabela
+				strcpy(tabelaEscolhida, nomeTabela);										//Guarda-se o nome da tabale escolhida
+				dados = diretorioDados(nomeTabela);											//Obtemos o respectivo diretório do arquivo com os dados da tabale
+				tamanho = diretorioTamanhos(nomeTabela);								//Obtemos o respectivo diretório do arquivo com os tamanhos dos tipos de dados da tabela
 				linha_atual++;
 				continue;
 			}
@@ -121,7 +120,7 @@ int pesquisarValor(){
 	free(nomeTabela);
 	nomeTabela = NULL;
 
-	/*Contagem da quantidade de cada tipo de dados da tabela*/
+	/*--Contagem da quantidade de cada tipo de dados da tabela--*/
 	pont_tamanho_tabela = fopen(tamanho, "r");
 	if (pont_tamanho_tabela == NULL){
 		printf("ERRO! O arquivo de tamanhos não foi aberto.\n");
@@ -149,7 +148,10 @@ int pesquisarValor(){
 	}
 
 	fclose(pont_tamanho_tabela);
-	/*Define estrutura nova linha, que possui arrays que guardam os atributos da linha a partir dos tipos de dados*/
+
+	/*--Define estrutura nova linha, que possui arrays
+			que guardam os atributos da linha a partir dos
+			tipos de dados*/
 	typedef struct linha Nova_Linha;
 	struct linha{
 		int inteiros[_inteiro];
@@ -159,7 +161,7 @@ int pesquisarValor(){
 	};
 	Nova_Linha nl;
 
-	/*Contadores dos tipos de dados*/
+	/*--Contadores dos tipos de dados--*/
 	_inteiro = 0;
 	_double = 0;
 	_char = 0;
@@ -170,12 +172,13 @@ int pesquisarValor(){
 	printf("Pesquisando valor na tabela '%s'!\n", tabelaEscolhida);
 	printf("Segue a lista de atributos:\n\n");
 
+	/*--Lista dos atributos da tabela solicitada--*/
 	do{
 		pont_tamanho_tabela = fopen(tamanho, "r");
 		if (pont_tamanho_tabela == NULL){
 			printf("ERRO! O arquivo de tamanhos não foi aberto.\n");
 			exit(0);
-		}else{																			//Havendo linhas, são listados os nomes das tabelas com um indice ao lado
+		}else{
 			ordem = 0;
 			while (fread(&atr, sizeof(Tamanho_Atributo), 1, pont_tamanho_tabela) == 1 ) {//Leitura enquanto houver elementos da estrutura tamanho de atributo da respectiva tabela
 				printf("%d. %s\n", ++ordem, atr.nome);
@@ -195,8 +198,8 @@ int pesquisarValor(){
 			}
 		}
 	} while (linha_escolhida < 1 || linha_escolhida > ordem);
-	
-	/*Localização dos arquivos da tabela solicidata*/
+
+	/*--Localização do nome do atributo da tabela solicidata--*/
 	pont_tamanho_tabela = fopen(tamanho, "r");
 	if (pont_tamanho_tabela == NULL){
 		printf("ERRO! O arquivo de tamanhos não foi aberto.\n");
@@ -219,7 +222,8 @@ int pesquisarValor(){
 	printf("---PESQUISAR VALOR--\n\n");
 	printf("Pesquisando valores de '%s' na tabela '%s'!\n", nomeAtributoEscolhido, tabelaEscolhida);
 	printf("Escolha uma das opções: \n\n");
-	
+
+	/*--Lista das opções de pesquisas--*/
 	if (tipoAtributoEscolhido == 4 || tipoAtributoEscolhido == 8){
 		do{
 			printf("1. valores maior que o valor informado\n");
@@ -230,7 +234,7 @@ int pesquisarValor(){
 			fflush(stdin);
 			printf("\nOpção: ");
 			scanf("%d", &linha_escolhida);
-			if (linha_escolhida < 1 || linha_escolhida > 5) {							//Controle de que o indice solicitado seja um dos exibidos na tela
+			if (linha_escolhida < 1 || linha_escolhida > 5) {													//Controle de que o indice solicitado seja um dos exibidos na tela
 				cabecalho();
 				printf("---PESQUISAR VALOR--\n\n");
 				printf("Pesquisando valores de '%s' na tabela '%s'!\n", nomeAtributoEscolhido, tabelaEscolhida);
@@ -243,6 +247,9 @@ int pesquisarValor(){
 		printf("---PESQUISAR VALOR--\n\n");
 		printf("Pesquisando valores de '%s' na tabela '%s'!\n\n", nomeAtributoEscolhido, tabelaEscolhida);
 
+		/*--Será verificada a opção que foi escolhida pelo usuário,
+				bem como o tipo de dados do mesmo. Daí, seŕa solicitado
+				o valor e exibido os dados que correspondem a pesquisa--*/
 		switch(linha_escolhida){
 			case 1:
 			pont_dados_tabela = fopen(dados, "r");
